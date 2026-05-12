bb77d1e6ef307b789fd06b0ff9e83accc9f4939e
Arthur Pereira <arthurpsouza502@gmail.com>
2026-05-12 13:00:05 -0300

    find funcionando

--- index.py
+++ index.py
@@ -1,3 +1,7 @@
 from deepface import DeepFace
+import cv2
 
-resultado = DeepFace.find('lula2.webp','db',batched=True)
+resultado = DeepFace.find(img_path='testes\DembeleTeste.webp',db_path='banco')
+
+for x in range(len(resultado)):
+    print(resultado[x])

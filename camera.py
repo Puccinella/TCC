591aadd5fe3be7c623d6f17ecd05651f0d77b878
Arthur Pereira <arthurpsouza502@gmail.com>
2026-05-15 18:21:46 -0300

    interface gráfica da camera
    tá meio porcão ainda mas servirá por enquanto

--- camera.py
+++ camera.py
@@ -1,18 +1,50 @@
 import cv2
+import tkinter as tk
 import os
+import sys
+from datetime import datetime
+import time
 
 cam = cv2.VideoCapture(0)
-
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+nomeV=''
 cv2.namedWindow("gravar")
 
+def fechar():
+    sys.exit()
+
+def inserir():
+    global nomeV
+    nomeV=entrada.get()
+    root.destroy()
+
+root = tk.Tk()
+root.geometry('300x200')
+root.title('Registro de estudante.')
+aviso = tk.Label(root, text='Insira o JC do estudante!',font=('helvetica',16))
+aviso.pack()
+
+entrada = tk.Entry(root)
+entrada.pack()
+
+Bfechar=tk.Button(root,command=fechar,text=('Cancelar'))
+Bfechar.pack(side='right')
+
+Bconfirma=tk.Button(root,command=inserir,text=('confirmar'))
+Bconfirma.pack(side='right')
+
+root.mainloop()
+
 img_counter = 0
-nomeV=str(input("insira o JC do estudante: "))
+
+
+
 caminho = 'banco/'+nomeV
 try:
     os.makedirs(caminho,exist_ok=True)
 except FileExistsError:
     print("!este jc já foi registrado!")
-
 else:
     pass
 
@@ -25,25 +57,29 @@ while True:
     if not ret:
         print("B.O")
         break
-    cv2.imshow("gravar", frame)
+    
 
+
+    frame = cv2.putText(frame,'Esc - Sair',(40,40),cv2.FONT_HERSHEY_DUPLEX,1,(0,0,0),2)
+    frame = cv2.putText(frame,'Espaco - Tirar foto',(40,80),cv2.FONT_HERSHEY_DUPLEX,1,(0,0,0),2)
+    frame = cv2.putText(frame,'Aluno: '+ nomeV,(800,40),cv2.FONT_HERSHEY_DUPLEX,1,(0,0,0),2)
+    frame = cv2.putText(frame,'Nº de fotos tiradas: '+ str(img_counter),(800,80),cv2.FONT_HERSHEY_DUPLEX,1,(0,0,0),2)
+    frame = cv2.putText(frame,datetime.now().strftime("%d/%m/%Y %H:%M:%S"),(850,620),cv2.FONT_HERSHEY_DUPLEX,1,(0,0,0),2)
 
     k = cv2.waitKey(1)
- 
+    
     if k%256 == 27:
         print("Escape hit, closing...")
         break
     elif k%256 == 32:
         nome = nome.strip('1234567890')+str(img_counter)
-        
-        print(nome)
         cv2.imwrite(nome+'.png',frame)
-
-        os.rename(nome+'.png',caminho+'/'+nome+'.png')
-
+        os.replace(nome+'.png',caminho+'/'+nome+'.png')
         img_counter += 1
+        cv2.imshow("gravar", frame)
+        time.sleep(0.67)
 
-
+    cv2.imshow("gravar", frame)
 
 cam.release()
 
--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 from deepface import DeepFace
 import cv2
 
-resultado = DeepFace.find(img_path='testes/rockTeste.png',db_path='banco')
+resultado = DeepFace.find(img_path='testes/wagnerTeste.jpg',db_path='banco')
 
 for x in range(len(resultado)):
     print(resultado[x])

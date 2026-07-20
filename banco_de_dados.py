046ddec9b71d42d4475db56fb1a098f3f2286525
Lucas1-72 <lucassandrade218@gmail.com>
2026-07-20 12:52:21 -0300

    banco de dados

--- banco_de_dados.py
+++ banco_de_dados.py
@@ -1,7 +1,20 @@
 #lembrem de instalar o psycopg2-binary para funcionar o banco de dados
 #lembrem também de instalar o postgresql, criar o banco de dados, as tabelas configurar certo
 #e colocar tudo certo no "conectar"
-
+#no pgadmin usei os comandos para criar as tabelas:
+#CREATE TABLE pessoas (
+#    id SERIAL PRIMARY KEY,
+#    prontuario VARCHAR(20) UNIQUE NOT NULL
+#);
+#CREATE TABLE embeddings (
+#    id SERIAL PRIMARY KEY,
+#    pessoa_id INTEGER NOT NULL,
+#    embedding DOUBLE PRECISION[] NOT NULL,
+#    CONSTRAINT fk_pessoa
+#        FOREIGN KEY (pessoa_id)
+#        REFERENCES pessoas(id)
+#        ON DELETE CASCADE
+#);
 import psycopg2
 
 def conectar():
@@ -14,10 +27,21 @@ def conectar():
     )
     return conn
 
-def cadastro(jc, embedding):
+def cadastro(jc):
     conn = conectar()
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO pessoas (prontuario, embedding) VALUES (%s, %s)", (jc, embedding))
+    cursor.execute("INSERT INTO pessoas (prontuario) VALUES (%s) RETURNING id", (jc,))
+    pessoa_id = cursor.fetchone()[0]
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+    return pessoa_id
+
+def adicionar_foto(pessoa_id, embedding):
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO embeddings (pessoa_id, embedding) VALUES (%s, %s)", (pessoa_id, embedding))
     conn.commit()
 
     cursor.close()
@@ -42,10 +66,21 @@ def deletar_pessoa(jc):
     cursor.close()
     conn.close()
 
+#se pa que isso vai ser a chave do nosso reconhecimento facial ->
+def buscar_todos_embeddings():
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute("SELECT p.prontuario, e.embedding FROM pessoas p JOIN embeddings e ON p.id = e.pessoa_id")
+
+    dados_pro_reconhecimento = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return dados_pro_reconhecimento
+
+
 conn = conectar()
 print ("deu certo")
 
 #teste do cadastro
-cadastro("JC303020",[0.1, 0.2, 0.3, 0.4, 0.5])  
-pessoa = buscar_pessoa("JC303020")
-print("Pessoa encontrada:", pessoa)
